1d5bae17be87367c2659bfa9cf3d65b9575b91f8
胡宏 <huhong.leif@bytedance.com>
2022-04-30 21:44:09 +0800

    2022-4-29

---
+++ src/leetcode/l_11/Solution.java
@@ -0,0 +1,11 @@
+package leetcode.l_11;
+
+class Solution {
+    final static int N = 100010;
+    static int[] l = new int[N];
+    static int[] r = new int[N];
+
+    public int maxArea(int[] height) {
+
+    }
+}
