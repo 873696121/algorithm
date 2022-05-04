d5c55475675785c65a4d6d6adee40978ee1e002c
hong <873696121@qq.com>
2022-05-04 20:09:49 +0800

    modify README.md

---
+++ src/leetcode/l_100/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.l_100;
+
+import leetcode.l_94.TreeNode;
+
+/***********************************************************                                          *
+ * Time: 2022/5/4
+ * Author: HuHong
+ * Desc: 相同的树
+ ***********************************************************/
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
