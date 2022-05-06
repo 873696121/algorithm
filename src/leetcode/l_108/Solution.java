8f05481c7de503d44621d76c98a4635d0032b352
hong <873696121@qq.com>
2022-05-06 23:36:33 +0800

    2022-05-06

---
+++ src/leetcode/l_108/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.l_108;
+
+import leetcode.l_94.TreeNode;
+
+/***********************************************************                                          *
+ * Time: 2022/5/6
+ * Author: HuHong
+ * Desc: 将有序数组转换为二叉搜索树
+ ***********************************************************/
+// todo
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return null;
+    }
+}
---
+++ src/leetcode/l_109/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.l_109;
+
+import leetcode.l_2.ListNode;
+import leetcode.l_94.TreeNode;
+
+/***********************************************************                                          *
+ * Time: 2022/5/6
+ * Author: HuHong
+ * Desc: 有序链表转换二叉搜索树
+ ***********************************************************/
+// todo
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        return null;
+    }
+}
