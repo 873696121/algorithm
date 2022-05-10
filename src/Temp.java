03349f23e060acfe6db6282531f54a654df0ab23
hong <873696121@qq.com>
2022-05-11 01:05:09 +0800

    2022-05-11

---
+++ src/Temp.java
@@ -0,0 +1,13 @@
+import java.util.BitSet;
+
+/***********************************************************                                          *
+ * Time: 2022/5/10
+ * Author: HuHong
+ * Desc:
+ ***********************************************************/
+public class Temp {
+    public static void main(String[] args) {
+        BitSet bitSet = new BitSet();
+        System.out.println(bitSet.get(33333));
+    }
+}
---
+++ src/leetcode/l_151/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.l_151;
+
+import java.util.Arrays;
+import java.util.List;
+
+/***********************************************************                                          *
+ * Time: 2022/5/11
+ * Author: HuHong
+ * Desc: 颠倒字符串中的单词
+ ***********************************************************/
+class Solution {
+    public String reverseWords(String s) {
+        String[] split = s.split(" ");
+        StringBuffer sb = new StringBuffer();
+        for(int i = split.length - 1; i >= 0; i --){
+            if(split[i].length() > 0 && !" ".equals(split[i])) {
+                sb.append(split[i] + " ");
+            }
+        }
+        return sb.toString().trim();
+    }
+}
